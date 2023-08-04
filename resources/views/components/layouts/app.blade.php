22e32fa46cb2469d71c98f635ae5b99cacb32280
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2023-08-04 18:00:33 +0545

    commented vite assest

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
  
         <title>{{ $title ?? 'Livewire v3 Todo' }}</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
         <div id="head" class="flex border-blue-800 border-t-2">
